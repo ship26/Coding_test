e9d12336b455f16dfa157ea6354b60c1c4de5536
ship26 <86402582+ship26@users.noreply.github.com>
2022-05-31 02:24:57 +0900

    2022-05-31

---
+++ BJ_1026.py
@@ -0,0 +1,16 @@
+N= int(input())
+
+S=0
+A= list(map(int,input().split()))
+B= list(map(int,input().split()))
+
+sorted_B = B.copy()
+sorted_B.sort()
+
+sorted_A = A.copy()
+sorted_A.sort(reverse=True)
+
+for i in range(N):
+    S +=sorted_A[i]*sorted_B[i]
+
+print(S)
